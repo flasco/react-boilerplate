8c8065c7b345223483f349086d96dceafc96fe7a
czq <chenzhengqiang@youzan.com>
2019-07-03 10:12:55 +0800

    init

---
+++ src/pages/home/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './index.scss';
+
+class Home extends React.PureComponent {
+  render() {
+    return (
+      <div style={{ padding: 20 }}>
+        <span>hello.</span>
+      </div>
+    );
+  }
+}
+
+export default Home;
